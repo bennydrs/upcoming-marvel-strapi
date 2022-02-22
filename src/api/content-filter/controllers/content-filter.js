7ce8a3fb087e276a1bdd77d2778e10df49734b77
benny <bennydrm16@gmail.com>
2022-02-22 09:54:20 +0700

    try fix bug sort in production

--- src/api/content-filter/controllers/content-filter.js
+++ src/api/content-filter/controllers/content-filter.js
@@ -42,39 +42,39 @@ module.exports = {
     const currentdate = new Date().toISOString().slice(0, 10);
     const dateOffset = 24 * 60 * 60 * 1000 * 1;
 
-    const data = entries
-      .filter((data) =>
-        data.ReleaseDate && data.ReleaseDate.date
-          ? new Date(
-              new Date(uniteReleaseDate(data)).toISOString().slice(0, 10)
-            ).getTime() +
-              dateOffset >=
-            new Date(currentdate).getTime()
-          : data
-      )
-      .sort((a, b) => {
-        let AtoDateFormat = uniteReleaseDate(a);
-        let BtoDateFormat = uniteReleaseDate(b);
+    const data = entries.filter((data) =>
+      data.ReleaseDate && data.ReleaseDate?.date
+        ? new Date(
+            new Date(uniteReleaseDate(data)).toISOString().slice(0, 10)
+          ).getTime() +
+            dateOffset >=
+          new Date(currentdate).getTime()
+        : data
+    );
 
-        let secondReleaseDate = a?.ReleaseDate?.date
-          ? new Date(AtoDateFormat).getTime() / 1000 - Date.now() / 1000
-          : 0;
-        let secondReleaseDateB = b?.ReleaseDate?.date
-          ? new Date(BtoDateFormat).getTime() / 1000 - Date.now() / 1000
-          : 0;
+    const dataSorted = data.sort((a, b) => {
+      let AtoDateFormat = uniteReleaseDate(a);
+      let BtoDateFormat = uniteReleaseDate(b);
 
-        // equal items sort equally
-        if (secondReleaseDate === secondReleaseDateB) {
-          return 0;
-        } else if (!a?.ReleaseDate?.date) {
-          return 1;
-        } else if (!b?.ReleaseDate?.date) {
-          return -1;
-        } else {
-          return secondReleaseDate < secondReleaseDateB ? -1 : 1;
-        }
-      });
+      let secondReleaseDate = a?.ReleaseDate?.date
+        ? new Date(AtoDateFormat).getTime() / 1000 - Date.now() / 1000
+        : 0;
+      let secondReleaseDateB = b?.ReleaseDate?.date
+        ? new Date(BtoDateFormat).getTime() / 1000 - Date.now() / 1000
+        : 0;
 
-    ctx.body = data.filter((d) => d.categories.length !== 0);
+      // equal items sort equally
+      if (secondReleaseDate === secondReleaseDateB) {
+        return 0;
+      } else if (!a?.ReleaseDate?.date) {
+        return 1;
+      } else if (!b?.ReleaseDate?.date) {
+        return -1;
+      } else {
+        return secondReleaseDate < secondReleaseDateB ? -1 : 1;
+      }
+    });
+
+    ctx.body = dataSorted.filter((d) => d.categories.length !== 0);
   },
 };
