bfa850f215d76d319c706f47d322b9a0540292af
benny <bennydrm16@gmail.com>
2022-04-13 08:57:39 +0700

    add cache with redis

--- config/plugins.js
+++ config/plugins.js
@@ -18,5 +18,49 @@ module.exports = ({ env }) => {
     };
   }
 
-  return {};
+  if (env("NODE_ENV") === "production" || env("NODE_ENV") !== "production") {
+    return {
+      // cache with redis
+      redis: {
+        config: {
+          connections: {
+            default: {
+              connection: {
+                // host: "127.0.0.1",
+                // port: 6379,
+                host: env("REDIS_HOST"),
+                port: env("REDIS_PORT"),
+                name: env("REDIS_NAME"),
+                password: env("REDIS_PASSWORD"),
+                db: 0,
+              },
+              settings: {
+                debug: false,
+              },
+            },
+          },
+        },
+      },
+      // Configure the redis cache plugin
+      "rest-cache": {
+        config: {
+          provider: {
+            name: "redis",
+            options: {
+              max: 32767 * 30,
+              connection: "default",
+            },
+          },
+          strategy: {
+            contentTypes: [
+              // list of Content-Types UID to cache
+              "api::category.category",
+              "api::content.content",
+              "api::genre.genre",
+            ],
+          },
+        },
+      },
+    };
+  }
 };
