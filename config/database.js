a0fd3ca9bcb9822108e9eb2fecfc16dd2ec29ce0
benny <bennydrm16@gmail.com>
2022-06-07 14:59:36 +0700

    config.port

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => {
         client: "postgres",
         connection: {
           host: config.host,
-          port: config.port,
+          port: config.port ? config.port : 5432,
           database: config.database,
           user: config.user,
           password: config.password,
@@ -33,6 +33,8 @@ module.exports = ({ env }) => {
       debug: false,
     },
   };
+  // postgres://krhxzmcq:d9ER95GYvPNGkIzQEJ0rEBHn1nBE2ElN@rosie.db.elephantsql.com/krhxzmcq
+  // postgres://vrqjohhmvvkmex:0bb4da251706bbaebc1c2f8e58e2fd2484ba583a097ce6e5203c8c3dc2897764@ec2-34-205-209-14.compute-1.amazonaws.com:5432/d9ejcpb8r6gm1p
   // return {
   //   connection: {
   //     client: "sqlite",
