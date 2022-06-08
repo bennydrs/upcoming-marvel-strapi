07c0239e65038b0953aeee4a8f664e3971fba677
benny <bennydrm16@gmail.com>
2022-06-08 10:24:29 +0700

    fixed config plugins

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,44 @@
-module.exports = {
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  redis: {
+    config: {
+      connections: {
+        default: {
+          connection: {
+            host: env("REDIS_HOST", "127.0.0.1"),
+            port: env("REDIS_PORT", 6379),
+            name: env("REDIS_NAME"),
+            password: env("REDIS_PASSWORD"),
+            db: 0,
+          },
+          settings: {
+            debug: false,
+          },
+        },
+      },
+    },
+  },
+  // Configure the redis cache plugin
   "rest-cache": {
     config: {
       provider: {
-        name: "memory",
+        name: "redis",
         options: {
           max: 32767,
-          maxAge: 3600,
+          connection: "default",
         },
       },
       strategy: {
@@ -18,4 +51,4 @@ module.exports = {
       },
     },
   },
-};
+});
